4f80ba0c79a32b39673f1652c5efaffb8d3e3cf1
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2022-09-07 12:56:27 -0600

    Consolidate into compose files

--- install.sh
+++ install.sh
@@ -51,8 +51,10 @@ export PIAUSER=$PIAUSER
 export PIAPASS=$PIAPASS
 export LOCALNET=$LOCALNET
 
-sudo rm ./components/.env
-cat > ./components/.env << EOF1
+#sudo rm ./frontend/.env
+#sudo rm ./backend/.env
+#sudo rm ./infrastructure/.env
+cat > ./frontend/.env << EOF1
 DOMAINNAME=$DOMAINNAME
 DOCKERPATH=$DOCKERPATH
 OAUTHCLIENT=$OAUTHCLIENT
@@ -67,184 +69,55 @@ PIAPASS=$PIAPASS
 LOCALNET=$LOCALNET
 EOF1
 
+cat > ./backend/.env << EOF1
+DOMAINNAME=$DOMAINNAME
+DOCKERPATH=$DOCKERPATH
+OAUTHCLIENT=$OAUTHCLIENT
+OAUTHSECRET=$OAUTHSECRET
+AUTHORIZEDUSERS=$AUTHORIZEDUSERS
+CERTCONTACT=$CERTCONTACT
+SECRETKEY=$SECRETKEY
+MEDIAPATH=$MEDIAPATH
+PLEXCLAIM=$PLEXCLAIM
+PIAUSER=$PIAUSER
+PIAPASS=$PIAPASS
+LOCALNET=$LOCALNET
+EOF1
+
+cat > ./infrastructure/.env << EOF1
+DOMAINNAME=$DOMAINNAME
+DOCKERPATH=$DOCKERPATH
+OAUTHCLIENT=$OAUTHCLIENT
+OAUTHSECRET=$OAUTHSECRET
+AUTHORIZEDUSERS=$AUTHORIZEDUSERS
+CERTCONTACT=$CERTCONTACT
+SECRETKEY=$SECRETKEY
+MEDIAPATH=$MEDIAPATH
+PLEXCLAIM=$PLEXCLAIM
+PIAUSER=$PIAUSER
+PIAPASS=$PIAPASS
+LOCALNET=$LOCALNET
+EOF1
 
 ###############################
-
 
 ##########   Docker ###########
-while true; do
-    read -p "Install Docker?" yn
-    case $yn in
-        [Yy]* ) sh ./components/docker.sh ; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
+sh ./docker.sh
 ###############################
+
+########.Create Docker Networks .#######
 sudo docker network create -d bridge --subnet=172.19.0.0/24 internal 
 sudo docker network create -d bridge --subnet=172.20.0.0/24 external
-######### Watchtower #############
-docker-compose -f ./components/watchtower.yaml up -d
-
 ###############################
 
-######### Portainer #############
+##########   Traefik Configure ###########
+#sh ./traefik.sh
+########################################
 
-while true; do
-    read -p "Install Portainer?" yn
-    case $yn in
-        [Yy]* ) docker-compose -f ./components/portainer.yaml up -d ; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-
-###############################
-
-######### Traefik #############
-
-while true; do
-    read -p "Install Traefik?" yn
-    case $yn in
-        [Yy]* ) sh ./components/traefik.sh ; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-###############################
-
-
-
-
-
-
-######### Netdata #############
-
-while true; do
-    read -p "Install Netdata?" yn
-    case $yn in
-        [Yy]* ) docker-compose -f ./components/netdata.yaml up -d ; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-###############################
-
-######### Plex #############
-
-while true; do
-    read -p "Install Plex?" yn
-    case $yn in
-        [Yy]* ) docker-compose -f ./components/plex.yaml up -d ; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-###############################
-
-######### NZBGet ##############
-
-while true; do
-    read -p "Install NZBGet?" yn
-    case $yn in
-        [Yy]* ) docker-compose -f ./components/nzbget.yaml up -d  ; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-###############################
-
-
-
-
-######### Sonar ###############
-
-while true; do
-    read -p "Install Sonarr?" yn
-    case $yn in
-        [Yy]* ) docker-compose -f ./components/sonarr.yaml up -d ; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-###############################
-
-######### Radarr ##############
-
-while true; do
-    read -p "Install Radarr?" yn
-    case $yn in
-        [Yy]* )docker-compose -f ./components/radarr.yaml up -d ; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-###############################
-
-######### Bazarr ##############
-
-while true; do
-    read -p "Install Bazarr?" yn
-    case $yn in
-        [Yy]* ) docker-compose -f ./components/bazarr.yaml up -d  ; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-###############################
-
-
-
-###### Overseerr #########
-
-while true; do
-    read -p "Install Overseerr?" yn
-    case $yn in
-        [Yy]* ) docker-compose -f ./components/overseerr.yaml up -d  ; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-###############################
-
-######## Speedtest ############
-
-while true; do
-    read -p "Install Speedtest?" yn
-    case $yn in
-        [Yy]* ) docker-compose -f ./components/speedtest.yaml up -d  ; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-###############################
-
-
-######### Tdarr #############
-
-while true; do
-    read -p "Install Tdarr?" yn
-    case $yn in
-        [Yy]* ) docker-compose -f ./components/tdarr.yaml up -d ; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-rm ./components/.env
-###############################
-
-echo "############################"
-echo "#######   All Done   #######"
-echo "############################"
+#########.Install Components.####
+#sudo docker-compose -f ./infrastructure/docker-compose.yaml up -data
+#sudo docker-compose -f ./frontend/docker-compose.yaml up -data
+#sudo docker-compose -f ./backend/docker-compose.yaml up -data
+#sudo rm ./frontend/.env
+#sudo rm ./backend/.env
+#sudo rm ./infrastructure/.env
