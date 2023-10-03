236a7e1e8159ec9d8a17165a9698230f9888313a
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2023-10-03 12:44:18 -0600

    added nextcloud

--- install.sh
+++ install.sh
@@ -80,6 +80,8 @@ PIAUSER=$PIAUSER
 PIAPASS=$PIAPASS
 LOCALNET=$LOCALNET
 DUCKDNSTOKEN=$DUCKDNSTOKEN
+NCDBROOT=$NCDBROOT
+NCDBUSER-$NCDBUSER
 EOF1
 
 cat > ./backend/.env << EOF1
@@ -130,9 +132,9 @@ sh ./traefik.sh
 ########################################
 
 #########.Install Components.####
-sudo docker-compose -f ./infrastructure/docker-compose.yaml up -d
-sudo docker-compose -f ./backend/docker-compose.yaml up -d
-sudo docker-compose -f ./frontend/docker-compose.yaml up -d
+sudo docker compose -f ./infrastructure/docker-compose.yaml up -d
+sudo docker compose -f ./backend/docker-compose.yaml up -d
+sudo docker compose -f ./frontend/docker-compose.yaml up -d
 sudo rm ./frontend/.env
 sudo rm ./backend/.env
 sudo rm ./infrastructure/.env
